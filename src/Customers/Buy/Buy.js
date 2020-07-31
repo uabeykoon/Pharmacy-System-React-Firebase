040e193d7e4975260c856ae3e7d2b0462bb1a6d3
udithaabeykoon <udithaedu@gmail.com>
2020-07-31 19:46:41 +0530

    orders

--- src/Customers/Buy/Buy.js
+++ src/Customers/Buy/Buy.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import { axiosAPI, axiosDB } from "../../Axios/Axios";
+import TableCus from "./TableCus/TableCus";
 
 class Buy extends Component {
 
     state = {
-        loading:false,
-        disableMedicine:false,
+        loading: false,
+        validateErrorMessage:false,
+        disableMedicine: false,
         pharmacies: [],
-        selectedPharmacy:null,
-        selectedMedicine:null,
-        selectedAmount:null,
-        allmedicineList:[],
-        medicineWithDetails:[],
-        medicineList:[]
+        selectedPharmacy: null,
+        selectedMedicine: null,
+        selectedAmount: null,
+        allmedicineList: [],
+        medicineWithDetails: [],
+        medicineList: [],
+        purchasingList: []
     };
 
 
@@ -21,41 +24,109 @@ class Buy extends Component {
         //this.fetchMedicine();
     }
 
-    onChangePharmacy =(event) =>{
+    onChangePharmacy = (event) => {
         this.setState({
-            loading:true,
-            selectedPharmacy:event.target.value
+            loading: true,
+            selectedPharmacy: event.target.value,
+            purchasingList:[]
         });
         this.fetchMedicine();
-        console.log(event.target.value);
+        //console.log(event.target.value);
+        //localStorage.setItem("id","new ID");
     }
-    onChangeMedicine = (event) =>{
-        console.log(event.target.value)
+    onChangeMedicine = (event) => {
+        //console.log(event.target.value)
         this.setState({
-            selectedMedicine:event.target.value
+            selectedMedicine: event.target.value
         });
     }
 
-    onChangeAmount= (event) =>{
+    onChangeAmount = (event) => {
         this.setState({
-            selectedAmount:event.target.value
+            selectedAmount: event.target.value
         });
     }
 
-    onAddtoListClick =(e) =>{
+    onAddtoListClick = (e) => {
+
         e.preventDefault();
-        let obj = {
-            medicineID:this.state.selectedMedicine,
-            amount:this.state.selectedAmount
-        };
-        console.log(obj)
+
+        //console.log(obj)
+
+        if(this.state.selectedAmount==="0" || this.state.selectedAmount===null || this.state.selectedPharmacy==="0" || this.state.selectedPharmacy===null || this.state.selectedMedicine==="0" || this.state.selectedMedicine===null){
+            this.setState({
+                validateErrorMessage:true
+            });
+        }else{
+
+            //console.log(this.findAmountOfmedicine(this.state.selectedMedicine));
+
+
+            let obj = {
+                medicineID: this.findRelatedObject(this.state.allmedicineList,this.state.selectedMedicine),
+                amount: parseInt(this.state.selectedAmount),
+                totalPricePerItem:this.findRelatedObject(this.state.allmedicineList,this.state.selectedMedicine).price*this.state.selectedAmount
+            };
+            //console.log(obj)
+
+            let array=[];
+            array=[...this.state.purchasingList,obj]
+            this.setState({
+                purchasingList:array
+            });
+
+        }
+
+
+
+
+    // puchToPurchasingList =() =>{
+    //     let array=[];
+    //     this.setState
+    // }
+
+
     }
+
+
+    onClickPlaceOrder =() =>{
+        //console.log(localStorage.getItem("id"));
+        //console.log("clicked")
+        if(this.state.purchasingList.length===0){
+            console.log("empty");
+        }else{
+            
+            let object = {
+                customerID:"-MDVbS0UfgjtYDGRXFch",
+                pharmacyID:this.state.selectedPharmacy,
+                medicines:this.state.purchasingList,
+                totalPrice:this.calculateTotalPrice(this.state.purchasingList)
+            };
+            this.addOrder(object)
+            .then((res)=>{
+                console.log(res);
+                console.log(this.props.match.url);
+                this.props.history.push("/customer/myorders")
+            }).catch((e)=>{
+                console.log(e);
+            })
+        }
+    }
+
+    findAmountOfmedicine = (medicineID) =>{
+        return this.state.medicineWithDetails.find((ob)=>ob.medicineID.id===medicineID).availableAmount
+    }
+    
 
 
     fetchPharmacy = () => {
+        this.setState({
+            loading: true
+        })
         axiosDB.get("pharmacy.json")
             .then((pharmacy) => {
                 this.setState({
+                    loading: false,
                     pharmacies: this.convertObjectToArray(pharmacy.data)
                 });
             }).catch((err) => {
@@ -66,6 +137,9 @@ class Buy extends Component {
     fetchMedicineRelatedToPharmacy = () => {
         return axiosDB.get(`pharmacyMedicine.json?orderBy="pharmacyID"&equalTo="${this.state.selectedPharmacy}"`)
 
+    }
+    addOrder =(object)=>{
+        return axiosDB.post("order.json",object);
     }
 
 
@@ -81,17 +155,17 @@ class Buy extends Component {
                         //console.log(medicine.data)
                         this.setState({
                             loading: false,
-                            medicineList:this.convertObjectToArray(medicine.data),
+                            medicineList: this.convertObjectToArray(medicine.data),
                             allmedicineList: this.convertObjectToArray(allMedicine.data)
-                           
+
                         });
                         this.setState({
-                            medicineWithDetails:this.combineMedicineDetails()
+                            medicineWithDetails: this.combineMedicineDetails()
                         })
                         //console.log(this.combineMedicineDetails());
 
-                        
-                    }).catch((err)=>{
+
+                    }).catch((err) => {
                         console.log(err);
                     })
 
@@ -103,17 +177,27 @@ class Buy extends Component {
             })
     }
 
-    combineMedicineDetails =() =>{
-        let array=[];
-        for(let med of this.state.medicineList){
-            array.push({...med,medicineID:this.findRelatedObject(this.state.allmedicineList,med.medicineID)});
+    combineMedicineDetails = () => {
+        let array = [];
+        for (let med of this.state.medicineList) {
+            array.push({ ...med, medicineID: this.findRelatedObject(this.state.allmedicineList, med.medicineID) });
         }
-        console.log(array);
+        //console.log(array);
         return array;
+
+       
     }
 
-    findRelatedObject = (array,id) =>{
-        return array.find((ob)=>ob.id===id);
+    calculateTotalPrice = (array) => {
+        let total = 0;
+        for (let ob of array) {
+            total = total + (ob.amount * ob.medicineID.price);
+        }
+        return total;
+    }
+
+    findRelatedObject = (array, id) => {
+        return array.find((ob) => ob.id === id);
     }
 
 
@@ -131,49 +215,57 @@ class Buy extends Component {
     render() {
 
         const spinner = this.state.loading ? (<><div className="sk-chase">
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-        <div className="sk-chase-dot"></div>
-    </div></>) : null;
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+        </div></>) : null;
+
+        const validateErrorMessage = this.state.validateErrorMessage ? (<div className="alert alert-danger" role="alert">
+            Input in Invalid Change it and try again
+        </div>) : null;
 
         return (
             <>
-            <form onSubmit={this.onAddtoListClick}>
-                <h1>Place your order here</h1><br />
-                {spinner}
-                <br />
-                <label>Pharmacy</label>
-                <select onChange={this.onChangePharmacy}>
-                <option value={0}>Select Pharmacy</option>
-                    {this.state.pharmacies.map((pharmacy) => {
-                        return (<option key={pharmacy.id} value={pharmacy.id}>{pharmacy.pharmacyName} - {pharmacy.pharmacyLocation}</option>);
-                    })}
-                </select>
-                <br />
+                <form onSubmit={this.onAddtoListClick}>
+                    <h1>Place your order here</h1><br />
+                    {spinner}
+                    <br />
+                    {validateErrorMessage}
+                    <label>Pharmacy</label>
+                    <select onChange={this.onChangePharmacy} required>
+                        <option value={0}>Select Pharmacy</option>
+                        {this.state.pharmacies.map((pharmacy) => {
+                            return (<option key={pharmacy.id} value={pharmacy.id}>{pharmacy.pharmacyName} - {pharmacy.pharmacyLocation}</option>);
+                        })}
+                    </select>
+                    <br />
 
-                <label>Medicine</label>
-                <select disabled={this.state.disableMedicine} onChange={this.onChangeMedicine}>
-                <option value={0}>Select Medicine</option>
-                    
-                {this.state.medicineWithDetails.map((medicine) => {
-                        return (<option key={medicine.medicineID.id} value={medicine.medicineID.id}>{medicine.medicineID.name}-{medicine.medicineID.dose}mg</option>);
-                    })}
-                </select>
-                <br />
+                    <label>Medicine</label>
+                    <select disabled={this.state.disableMedicine} onChange={this.onChangeMedicine} required>
+                        <option value={0}>Select Medicine</option>
 
-                <label>Quantity</label>
-                <input type="text" id="quantity" name="quantity" disabled={this.state.disableMedicine} onChange={this.onChangeAmount}/>
-                <br />
-                <input type="submit" value="Add to cart" disabled={this.state.disableMedicine} />
-                <input type="reset" value="Clear" disabled={this.state.disableMedicine} />
-                <br /><br /><br />
-                <h2>please attach a photo of prescription</h2>
-                <label>Select a image</label>
-                <input type="file" id="myfile" name="myfile"></input>
+                        {this.state.medicineWithDetails.map((medicine) => {
+                            return (<option key={medicine.medicineID.id} value={medicine.medicineID.id}>{medicine.medicineID.name}-{medicine.medicineID.dose}mg</option>);
+                        })}
+                    </select>
+                    <br />
+
+                    <label>Quantity</label>
+                    <input type="text" id="quantity" name="quantity" disabled={this.state.disableMedicine} onChange={this.onChangeAmount} required />
+                    <br />
+                    <input type="submit" value="Add to cart" disabled={this.state.disableMedicine} />
+                    <input type="reset" value="Clear" disabled={this.state.disableMedicine} />
+                    <br /><br /><br />
+                    <h2>please attach a photo of prescription</h2>
+                    <label>Select a image</label>
+                    <input type="file" id="myfile" name="myfile"></input>
                 </form>
+                <div>
+                    <TableCus medicineList={this.state.purchasingList} placeOrder={this.onClickPlaceOrder}/>
+                </div>
             </>
         );
     }
---
+++ src/Customers/Buy/TableCus/TableCus.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './TableCus.css';
+
+
+const TableCus = (props) => {
+    //console.log(props.medicineList)
+
+    return (
+        <>
+            {/* {props.input2} */}
+            <table id="customers">
+                <thead>
+                    <tr>
+                        <th>Madicine Name</th>
+                        <th>Dose</th>
+                        <th>Price Per Unit</th>
+                        <th>Amount</th>
+                        <th>Total Price Per Item</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.medicineList.map((medicine) => {
+                        return (<tr key={medicine.medicineID.id}>
+                            <td>{medicine.medicineID.name}</td>
+                            <td>{medicine.medicineID.dose}mg</td>
+                            <td>Rs {medicine.medicineID.price}/=</td>
+                            <td>{medicine.amount}</td>
+                            <td>Rs {medicine.amount * medicine.medicineID.price}/=</td>
+                        </tr>);
+                    })}
+                </tbody>
+            </table>
+
+            <h1>Total Price - Rs {calculateTotalPrice(props.medicineList)} /=</h1>
+            <button className="btn btn-primary" onClick={props.placeOrder}>Place Order</button>
+        </>
+    );
+
+}
+const calculateTotalPrice = (array) => {
+    let total = 0;
+    for (let ob of array) {
+        total = total + (ob.amount * ob.medicineID.price);
+    }
+    return total;
+}
+export default TableCus;
